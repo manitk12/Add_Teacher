6c97fd6d002c1fb2907a4024ae94372ff5227642
manit <ermanit007@gmail.com>
2017-07-26 15:16:55 +0530

    design teacher.xml added  button yes or no

---
+++ app/src/main/java/com/grademojo/add_teacher/Teacher.java
@@ -0,0 +1,13 @@
+package com.grademojo.add_teacher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Teacher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher);
+    }
+}
