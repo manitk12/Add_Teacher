39f9efe127bae8bb20a790a96b07a79304af5e75
manit <ermanit007@gmail.com>
2017-07-26 16:53:21 +0530

    In Teacher.java class set click on listener in Main cardview with the help of boolean and Relative layoutparam added in cardview

--- app/src/main/java/com/grademojo/add_teacher/Teacher.java
+++ app/src/main/java/com/grademojo/add_teacher/Teacher.java
@@ -2,12 +2,85 @@ package com.grademojo.add_teacher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class Teacher extends AppCompatActivity {
+
+
+    private CardView toogle_cover ,main_card_View;
+
+
+
+
+
+
+
+    private Boolean button_state_yes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+
+
+        toogle_cover = (CardView) findViewById(R.id.toggle_button);
+
+        main_card_View = (CardView) findViewById(R.id.edit_card_view_genger);
+
+
+        button_state_yes = true;
+
+        main_card_View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (button_state_yes)
+                {
+                    button_state_yes = false;
+
+
+                    RelativeLayout.LayoutParams lps = (RelativeLayout.LayoutParams) toogle_cover.getLayoutParams();
+
+                    lps.addRule(RelativeLayout.ALIGN_START, R.id.frame_yes);
+                    lps.addRule(RelativeLayout.ALIGN_END, R.id.frame_yes);
+                    lps.addRule(RelativeLayout.ALIGN_TOP, R.id.frame_yes);
+                    lps.addRule(RelativeLayout.ALIGN_BOTTOM, R.id.frame_yes);
+
+                    toogle_cover.setLayoutParams(lps);
+                }
+
+                else {
+
+                    button_state_yes = true;
+
+
+                    RelativeLayout.LayoutParams lps = (RelativeLayout.LayoutParams) toogle_cover.getLayoutParams();
+
+                    lps.addRule(RelativeLayout.ALIGN_START, R.id.frame_no);
+                    lps.addRule(RelativeLayout.ALIGN_END, R.id.frame_no);
+                    lps.addRule(RelativeLayout.ALIGN_TOP, R.id.frame_no);
+                    lps.addRule(RelativeLayout.ALIGN_BOTTOM, R.id.frame_no);
+
+                    toogle_cover.setLayoutParams(lps);
+
+
+
+                }
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
 }
